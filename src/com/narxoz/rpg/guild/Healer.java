9a7b48326b05c8b7f59e3087cf67c0c2fdddbddf
azizasabit-ui <aziza.sabi@narxoz.kz>
2026-05-09 00:05:54 +0500

    Add Healer colleagues

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,21 +1,33 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
 public class Healer extends GuildMember {
+    private int healingPrepared = 0;
 
     public Healer(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
+    }
+
+    public void prepareHealingPotions(int amount) {
+        healingPrepared += amount;
+        System.out.println("  🧪 " + getName() + " prepares " + amount + " healing potions!");
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch (topic) {
+            case "healing":
+                System.out.println("  💚 " + getName() + " receives healing request from " + from.getName());
+                prepareHealingPotions(30);
+                break;
+            case "aid":
+                System.out.println("  🩹 " + getName() + " prepares medical aid: " + payload);
+                break;
+            default:
+                System.out.println("  ℹ️ " + getName() + " noted message on [" + topic + "]: " + payload);
+        }
     }
 }
